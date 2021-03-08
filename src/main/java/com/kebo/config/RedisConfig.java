be72a15f8ee0607887eb73dff30b280a69e1648a
Administrator <249183617@qq.com>
2021-03-08 13:41:34 +0800

    2021年3月8日->加入连接redis的配置信息

---
+++ src/main/java/com/kebo/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.kebo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @description: 配置序列化器
+ * @Author: kb
+ * @Date: 2021-03-08 13:38
+ */
+@Configuration
+public class RedisConfig{
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String,Object>template=new RedisTemplate<>();
+        //关联
+        template.setConnectionFactory(factory);
+        //设置key的序列化器
+        template.setKeySerializer(new StringRedisSerializer());
+        //设置value的序列化器
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
+
--- src/test/java/com/kebo/SpringbootExamplesApplicationTests.java
+++ src/test/java/com/kebo/SpringbootExamplesApplicationTests.java
@@ -1,13 +1,20 @@
 package com.kebo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class SpringbootExamplesApplicationTests {
 
+	@Autowired
+	private RedisTemplate<String,Object> redisTemplate;
+
 	@Test
-	void contextLoads() {
+	public void set(){
+		redisTemplate.opsForValue().set("myKey","柯博");
+		System.out.println(redisTemplate.opsForValue().get("myKey"));
 	}
 
 }
